1b541f84715e24d03a1688db9916f4ec1193c86c
Spenser <spenser@pbhs.com>
2014-10-30 13:04:53 -0700

    renamed file to match folder, and updated plugin info and created setting page action

---
+++ wp-treehouse-badges.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ *	Plugin Name: Official Treehouse Badges Plugin
+ *	Plugin URI: http://wptreehouse.com/wptreehouse-badges-plugin/
+ *	Description: Provides both widgets and shortcodes to help you display your Treehouse profile badges on your website.  The official Treehouse badges plugin.
+ *	Version: 1.0
+ *	Author: Spenser Hale
+ *	Author URI: http://www.spenserhale.com
+ *	License: GPL2
+ *
+*/
+
+/*
+ *	Add a link to our plugin in the admin menu
+ *	under 'Settings > Treehouse Badges'
+ *
+*/
+
+function wptreehouse_badges_menu() {
+
+	/*
+	 * 	Use the add_options_page function
+	 * 	add_options_page( $page_title, $menu_title, $capability, $menu-slug, $function ) 
+	 *
+	*/
+
+	add_options_page(
+		'Official Treehouse Badges Plugin',
+		'Treehouse Badges',
+		'manage_options',
+		'wptreehouse-badges',
+		'wptreehouse_badges_options_page'
+	);
+
+}
+add_action( 'admin_menu', 'wptreehouse_badges_menu' );
+
+
+function wptreehouse_badges_options_page() {
+
+	if( !current_user_can( 'manage_options' ) ) {
+
+		wp_die( 'You do not have suggicient permissions to access this page.' );
+
+	}
+
+	echo '<p>Welcome to our plugin page!</p>';
+
+
+}
+
+?>
